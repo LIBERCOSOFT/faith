bee787c9df0d8abbf57fee31b5983d02b6634cc0
OLADIPO-K <akinspreciousola@gmail.com>
2021-03-01 23:46:36 +0100

    second commit

--- main/script.js
+++ main/script.js
@@ -1,27 +1,27 @@
-let first = () => {
-  document.getElementById("desc").innerText = "first";
-  // scroll indicator's background
-  document.getElementById("bar1").style.backgroundColor = "#fff";
-  document.getElementById("bar2").style.backgroundColor = "transparent";
-  // pictures opacity
-  document.getElementById("secondPhoto").style.opacity = "0.5";
-  document.getElementById("firstPhoto").style.opacity = "1";
-}
+$(document).ready(function () {
 
-let second = () => {
-  document.getElementById("desc").innerText = "second";
-  // scroll indicator's background
-  document.getElementById("bar2").style.backgroundColor = "#fff";
-  document.getElementById("bar1").style.backgroundColor = "transparent";
-  // pictures opacity
-  document.getElementById("firstPhoto").style.opacity = "0.5";
-  document.getElementById("secondPhoto").style.opacity = "1";
-}
+  $("#firstPhoto").hover(function () {
+    $("#desc").text("first");
+    $("#bar1").css("background-color", "#fff");
+    $("#bar2").css("background-color", "transparent");
+    $("#secondPhoto").css("opacity", "0.5");
+    $("#firstPhoto").css("opacity", "1");
+  });
 
-let reset = () => {
-  document.getElementById("desc").innerText = "Starter Text";
-  document.getElementById("bar1").style.backgroundColor = "transparent";
-  document.getElementById("bar2").style.backgroundColor = "transparent";
-  document.getElementById("firstPhoto").style.opacity = "1";
-  document.getElementById("secondPhoto").style.opacity = "1";
-}
+  $("#secondPhoto").hover(function () {
+    $("#desc").text("second");
+    $("#bar2").css("background-color", "#fff");
+    $("#bar1").css("background-color", "transparent");
+    $("#firstPhoto").css("opacity", "0.5");
+    $("#secondPhoto").css("opacity", "1");
+  });
+
+  $(".reset").mouseout(function () {
+    $("#desc").text("Starter text");
+    $("#bar1").css("background-color", "transparent");
+    $("#bar2").css("background-color", "transparent");
+    $("#firstPhoto").css("opacity", "1");
+    $("#secondPhoto").css("opacity", "1");
+  })
+
+});
